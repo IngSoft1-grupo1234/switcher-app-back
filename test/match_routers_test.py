88ff9adc3d6b6ab16b60c23bdf3ab5196a72fd84
Santiago Morales <78430747+SantiagoMoraless@users.noreply.github.com>
2024-10-13 15:16:50 -0300

    Update match_routers_test.py

--- test/match_routers_test.py
+++ test/match_routers_test.py
@@ -145,19 +145,22 @@ def test_pass_turn():
 def test_pass_turn_match_not_found():
     with patch('app.crud.match_crud.MatchRepository.pass_turn', return_value=None):
         with patch('app.crud.match_crud.MatchRepository.get_next_player', return_value=None):
-            with pytest.raises(HTTPException) as exc_info:
-                client.put("/matches/999/next_turn")
+            with patch('app.crud.match_crud.MatchRepository.get_player_ids_in_match', return_value=[]):
+                with pytest.raises(HTTPException) as exc_info:
+                    client.put("/matches/999/next_turn")
 
-            assert exc_info.value.status_code == 404
-            assert exc_info.value.detail == "Match not found."
+                assert exc_info.value.status_code == 404
+                assert exc_info.value.detail == "Match not found."
 
 
 def test_pass_turn_no_next_player():
     with patch('app.crud.match_crud.MatchRepository.pass_turn', return_value=None):
-        with patch('app.crud.match_crud.MatchRepository.get_next_player', return_value=None),\
-                patch('app.crud.match_crud.MatchRepository.get_player_ids_in_match', return_value=[1, 2]):
+        with patch('app.crud.match_crud.MatchRepository.get_next_player', return_value="Player1"),\
+            patch('app.crud.match_crud.MatchRepository.get_player_ids_in_match', return_value=[1, 2]):
+             
             response = client.put("/matches/1/next_turn")
-            assert response.status_code == 204
+            assert response.status_code == 204  
+
 
 
 
