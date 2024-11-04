7d71228b1619154294f6a60173bd08ea376eed1c
Bruno Storani <darkaraus@gmail.com>
2024-11-04 17:08:57 -0300

    test realmente completos.

--- test/match_crud_test.py
+++ test/match_crud_test.py
@@ -20,6 +20,7 @@ def player_mock_session():
     with patch('app.crud.movecard_crud.session', autospec=True) as movecard_mock_session:
         yield movecard_mock_session
 
+
 @pytest.fixture
 def match_repo(mock_session):
     return MatchRepository()
@@ -135,19 +136,18 @@ def test_pass_turn(match_repo, mock_session, player_mock_session):
     mock_db.query.return_value.get.return_value = mock_match
     mock_db.get.return_value = mock_match
 
-    def mock_get(model, id):
-            if model == PlayerModel:
-                return PlayerModel(player_id=1, username="Player1", match_id = 1, used_cards = json.dumps([]))
-            return None
-    mock_db.get.side_effect = mock_get
     
-    with patch('app.crud.match_crud.MatchRepository.get_player_ids_in_match', return_value=[1, 2]),\
-        patch('app.crud.shapecard_crud.ShapeCardRepository.get_amount_of_shape_cards_by_player', return_value=3),\
-        patch('app.crud.movecard_crud.MoveCardRepository.get_amount_of_move_cards_by_player', return_value=3):
-        player_mock_db = player_mock_session.return_value
-    player_mock_db.get.return_value = PlayerModel(player_id=1, username="Player1", match_id = 1, used_cards = json.dumps([]))
-    with patch('app.crud.movecard_crud.MoveCardRepository.confirm_moves', return_value=(pass_turn_return_board, pass_turn_return_shapes)):
-        result = match_repo.pass_turn(1, log=False)
+
+    with patch('app.crud.shapecard_crud.session', autospec=True) as shapecard_mock_session:
+        shapecard_mock_db = shapecard_mock_session.return_value
+        shapecard_mock_db.query.return_value.get.return_value = PlayerModel(player_id=1, username="Player1", match_id = 1, used_cards = json.dumps([]))
+        with patch('app.crud.match_crud.MatchRepository.get_player_ids_in_match', return_value=[1, 2]),\
+            patch('app.crud.shapecard_crud.ShapeCardRepository.get_amount_of_shape_cards_by_player', return_value=3),\
+            patch('app.crud.movecard_crud.MoveCardRepository.get_amount_of_move_cards_by_player', return_value=3):
+            player_mock_db = player_mock_session.return_value
+        player_mock_db.get.return_value = PlayerModel(player_id=1, username="Player1", match_id = 1, used_cards = json.dumps([]))
+        with patch('app.crud.movecard_crud.MoveCardRepository.confirm_moves', return_value=(pass_turn_return_board, pass_turn_return_shapes)):
+            result = match_repo.pass_turn(1, log=False)
 
     assert result == (pass_turn_return_board, pass_turn_return_shapes)
     assert mock_match.current_turn == 2
