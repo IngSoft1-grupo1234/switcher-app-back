80cb37529c54e4509e1407794e2cc03866b815fe
Bruno Storani <darkaraus@gmail.com>
2024-11-09 14:46:54 -0300

    Timer pasado a 10 minutos para testear develop tranquilamente, cambiar!

--- app/crud/match_crud.py
+++ app/crud/match_crud.py
@@ -447,7 +447,7 @@ class MatchRepository:
             self.timer_events[match_id].clear()
             try:
                 try:
-                    await asyncio.wait_for(self.timer_events[match_id].wait(), timeout=120)
+                    await asyncio.wait_for(self.timer_events[match_id].wait(), timeout=1200)
                 except asyncio.CancelledError:
                     break # Si la partida deja de existir...
             except asyncio.TimeoutError:
